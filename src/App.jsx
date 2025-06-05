9d88fb24cf6518b32bd51c131be3a6a026435afa
Alex <alejandrotorquemada@gmail.com>
2025-06-05 12:25:29 +0200

    New landpage style

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="grid grid-rows-[1fr_1fr] h-dvh">
+      <header className="h-auto flex flex-col m-auto items-center pt-30">
+        <section className=" flex flex-col items-center">
+          <article className="flex flex-col justify-center items-center gap-5">
+            <div className="flex flex-row">
+              <h1 className="text-6xl flex items-center">
+                Hi, I'm Alejandro Torquemada
+                <h1 className="text-6xl flex items-center animate-codebar s:hidden">
+                  |
+                </h1>
+              </h1>
+            </div>
+            <p className="text-2xl">Front End Developer</p>
+            <p className="flex flex-row gap-10">
+              <img src="src\assets\html.png" alt="HTML" className="w-10 h-10" />
+              <img src="src\assets\css.png" alt="CSS" className="w-10 h-10" />
+              <img src="src\assets\js.png" alt="JS" className="w-10 h-10" />
+              <img
+                src="src\assets\react.png"
+                alt="React"
+                className="w-10 h-10"
+              />
+              <img
+                src="src\assets\github.png"
+                alt="GitHub"
+                className="w-10 h-10"
+              />
+            </p>
+          </article>
+        </section>
+      </header>
+
+      <main className="h-[100%] flex flex-col m-auto pt-20 pb-20">
+        <section className="w-screen max-w-3xl flex flex-col gap-3 p-4">
+          <article className="flex flex-col gap-2">
+            <h2 className="text-3xl text-main">What i do</h2>
+            <ul className="text-m list-disc list-inside">
+              <li>Write HTML with a proper semantic</li>
+              <li>Find the best style in CSS</li>
+              <li>Create funcitions that helps and make the web interactive</li>
+              <li>RESTful APIs and components are the way to go</li>
+              <li>Always keep updated</li>
+            </ul>
+            <p className="text-xs self-end">
+              * Those are some of the things I am better at, but I can do more!
+            </p>
+          </article>
+        </section>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
